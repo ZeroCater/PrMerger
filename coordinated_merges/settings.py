30f5c0e6f300203641d564ffa7fc08a94030cc20
Theophile Nsengimana <theo@zerocater.com>
2017-01-06 07:10:31 -0800

    Add whitenoise

--- coordinated_merges/settings.py
+++ coordinated_merges/settings.py
@@ -35,6 +35,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -128,4 +129,5 @@ RQ_QUEUES = {
     }
 }
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
